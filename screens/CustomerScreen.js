5b958525fcb923d6c4b47aa62705be578359a605
annalisa4sec <lizmartinmusic@gmail.com>
2025-02-18 13:42:32 +0100

    add button and text input

--- screens/CustomerScreen.js
+++ screens/CustomerScreen.js
@@ -1,9 +1,45 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Alert,
+  Keyboard,
+  Pressable,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { styles } from "../styles";
 
 export const CustomerScreen = () => {
+  const [name, onChangeName] = useState("");
+  const handleAddCustomer = () => {
+    Alert.alert(
+      "Added!",
+      `Customer ${name} was added to list`,
+      [
+        {
+          text: "OK",
+          style: "default",
+        },
+      ],
+      { cancelable: true }
+    );
+  };
+
   return (
-    <View>
+    <View style={styles.formContainer}>
       <Text>Customer Screen</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="add customer name"
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={name}
+        onChangeText={onChangeName}
+        onBlur={Keyboard.dismiss}
+      />
+      <Pressable style={styles.button} onPress={handleAddCustomer}>
+        <Text style={styles.buttonText}>Save Customer</Text>
+      </Pressable>
     </View>
   );
 };
